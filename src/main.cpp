50bd5611432b633ba70edfb250f1d0c24847614f
VojtechKycelt <71277196+VojtechKycelt@users.noreply.github.com>
2024-09-16 01:27:44 +0200

    add renderer header, create loop of rectangle bumping to sides of window

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,54 @@
 #include <SFML/Graphics.hpp>
+#include "renderer.h"
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    //declare renderer with window
+    Renderer renderer(sf::Vector2i(1280, 720), "APOPHIS ENGINE EXPERIMENT", 240);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
+    //declare rectangle
+    sf::RectangleShape rect;
+    sf::Vector2f rectanglePosition(540,250);
+    sf::Vector2f rect_velocity(200.0, 200.0);
+    rect.setSize(sf::Vector2f(100, 100));
+    rect.setFillColor(sf::Color(250,250,250));
+    rect.setOutlineColor(sf::Color::White);
+    rect.setOutlineThickness(10);
+    rect.setPosition(rectanglePosition);
+
+    //keep track of time between frames
+    sf::Clock deltaClock;
+    float deltaTime;
+
+    //engine loop
+    while (renderer.window.isOpen()) {
+
+        //get delta time for same speed of the engine with different fps
+        deltaTime = deltaClock.restart().asSeconds();
+
+        //handle input
+        for (auto event = sf::Event{}; renderer.window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) renderer.window.close();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                rect_velocity.x *= 2;
+                rect_velocity.y *= 2;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                rect_velocity.x /= 2;
+                rect_velocity.y /= 2;
             }
         }
 
-        window.clear();
-        window.display();
+        //physics
+        if (rectanglePosition.x + rect_velocity.x * deltaTime < 0 || rectanglePosition.x + rect_velocity.x * deltaTime > renderer.WINDOW_SIZE.x - rect.getSize().x) rect_velocity.x *= -1;
+        if (rectanglePosition.y + rect_velocity.y * deltaTime < 0 || rectanglePosition.y + rect_velocity.y * deltaTime > renderer.WINDOW_SIZE.y - rect.getSize().y) rect_velocity.y *= -1;
+        rectanglePosition.x += rect_velocity.x * deltaTime;
+        rectanglePosition.y += rect_velocity.y * deltaTime;
+        rect.setPosition(rectanglePosition);
+
+        //display rectangle
+        renderer.render(rect);
     }
 }
+
+
---
+++ src/renderer.h
@@ -0,0 +1,31 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <SFML/Graphics.hpp>
+
+class Renderer {
+public:
+    //TODO explicit? wdym?
+    //default constructor?
+    explicit Renderer(const sf::Vector2i& window_size, const std::string& window_name, const int& fps_limit) : WINDOW_SIZE(window_size),
+        window_name_(window_name) {
+        window.setFramerateLimit(fps_limit);
+
+    };
+    const sf::Vector2i WINDOW_SIZE;
+    std::string window_name_;
+    sf::RenderWindow window = sf::RenderWindow(
+        sf::VideoMode(WINDOW_SIZE.x, WINDOW_SIZE.y),
+        window_name_
+    );
+
+    //TODO abstract param to render other objects (triangles/lines)
+    void render(const sf::RectangleShape& rect) {
+        window.clear(sf::Color::Green);
+        window.draw(rect);
+        window.display();
+    }
+
+};
+
+#endif //RENDERER_H
